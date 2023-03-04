106ca10de5ff4ea34752dcc4596782a5add7174e
KingCrimsonJY <kingcrimsonjy@163.com>
2023-03-04 13:18:42 +0800

    test

--- app/src/main/java/launcher/kcjy/xyz/variable.java
+++ app/src/main/java/launcher/kcjy/xyz/variable.java
@@ -2,7 +2,7 @@ package launcher.kcjy.xyz;
 
 public class variable {
     public static String url = "http://kcjy.xyz/update.php";
-    public static int nowversion = 1;
+    public static int nowversion = 2;
     public static String path = "/storage/emulated/0/Android/data/launcher.kcjy.xyz/files/update";
     public static String filename = path+"/update.apk";
 
